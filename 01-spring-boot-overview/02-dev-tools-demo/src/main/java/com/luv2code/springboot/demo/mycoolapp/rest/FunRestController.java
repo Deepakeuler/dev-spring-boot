e2230531ff688e1fffde0a79d9c27ea13c4c56f7
Deepak <deepak@MacBook-Pro.local>
2026-04-16 16:02:36 +0530

    added spring boot dev tools for auto reload

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -10,4 +10,15 @@ public class FunRestController {
     public String sayHello(){
         return "Hello World!";
     }
+
+    //sanity check for auto start
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky Day!";
+    }
 }
